6e296c7e42869f0cf3346a48bbb0afac01a9d1a7
StudentFPW <jofeleonids00@gmail.com>
2024-02-26 21:30:15 +0200

    26/02/2024

--- stripepay/stripepay/urls.py
+++ stripepay/stripepay/urls.py
@@ -14,9 +14,52 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="StripePay API",
+        default_version="v1",
+        description="StripePay description",
+        terms_of_service="https://t.me/LEON_JOFE",
+        contact=openapi.Contact(email="jofeleonids00@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
+
+# This exposes 4 endpoints:
+
+#   A JSON view of your API specification at /swagger.json
+#   A YAML view of your API specification at /swagger.yaml
+#   A swagger-ui view of your API specification at /swagger/
+#   A ReDoc view of your API specification at /redoc/
